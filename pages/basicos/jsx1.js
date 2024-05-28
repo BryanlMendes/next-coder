a2c0e12ac621615eac247674c41f2d641a2481cf
Bryan <bryanlucas_mendes@hotmail.com>
2024-05-27 22:08:38 -0300

    27-05-24

---
+++ pages/basicos/jsx1.js
@@ -0,0 +1,10 @@
+function jsx1(){
+    return(
+        <div>
+            <h1>
+                JSX #01
+            </h1>
+        </div>
+    )
+}
+export default jsx1;
---
+++ pages/basicos/jsx3.jsx
@@ -0,0 +1,11 @@
+function jsx3 (){
+    return(
+        <div className="container">
+            <h1>
+                JSX #03
+            </h1>
+        </div>
+    )
+
+}
+export default jsx3;
